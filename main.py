289b74c6bafd3dab15b52faeeaeb8eb9bb6c0cec
afuafu001 <108642323+afuafu001@users.noreply.github.com>
2025-04-04 18:44:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent, EventMessageType, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-
-from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.provider import LLMResponse
 
 # @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
